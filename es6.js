548f3847f04e6f6f30acad00205002366ef302cf
paper <jue_paper@qq.com>
2014-06-25 23:37:38 +0800

    0.1.1 format code

--- es6.js
+++ es6.js
@@ -3,12 +3,13 @@
   1. 只针对高级浏览器
   2. 不会做很复杂的判断用户输入
 */
+
 var ES6 = {}
 
 /**
   ------------------------------------------------------------------------
   字符串的扩展  
-  
+
   1. contains(), startsWith(), endsWith()
   2. repeat()
   3. template string
@@ -17,14 +18,14 @@ var ES6 = {}
 
 String.prototype.contains = function(str, atIndex){
   atIndex = atIndex || 0;
-  
+
   return this.indexOf(str, atIndex) > -1;
 }
 
 String.prototype.startsWith = function(str, atIndex){
   atIndex = atIndex || 0;
   var index = this.indexOf(str, atIndex);
-  
+
   return index == atIndex;
 }
 
@@ -33,9 +34,9 @@ String.prototype.startsWith = function(str, atIndex){
 String.prototype.endsWith = function(str, atIndex){
   var thisLen = this.length;
   var strLen = str.length;
-  
+
   atIndex = atIndex || 0;
-  
+
   if( atIndex != 0 ){
     atIndex = thisLen - atIndex;
   }
@@ -54,106 +55,94 @@ String.prototype.repeat = function(num){
       result += this;
     }
   }
-  
+
   return result;
 }
 
 /**
-	var name = "Bob", time = "today";
-	var r = ES6.templateString('Hello ${name}, how are you ${time}?');
-	
-	这种太难实现
-	因为字符串里面的 name, time 不知道是取局部变量的，还是全局变量的。
-	比如：
-	
-	var name = "paper", time = "tomorrow";
-	;(function(){
-		var name = "Bob", time = "today";
-		var r = ES6.templateString('Hello ${name}, how are you ${time}?');
-	})()
-	
-	r 应该返回的是：Hello Bob, how are you today?'
-	怎么才能让 templateString 函数取局部变量的值呢？？
-	
-	-----------------------------------------
-	
-	所以目前的API是：
-	var name = "Bob", time = "today";
-	var r1 = ES6.templateString({
-		name : name,
-		time : time
-	},'Hello ${name}, how are you ${time}?');
-	=> 'Hello Bob, how are you today?'
-	
-	var x = 1, y = 2;
-	var r2 = ES6.templateString({
-		x : x,
-		y : y
-	},'${ x } + ${ y } = ${ x + y }');
-	=> 1 + 2 = 3
+  var name = "Bob", time = "today";
+  var r = ES6.templateString('Hello ${name}, how are you ${time}?');
+
+  这种太难实现
+  因为字符串里面的 name, time 不知道是取局部变量的，还是全局变量的。
+  比如：
+
+  var name = "paper", time = "tomorrow";
+  ;(function(){
+    var name = "Bob", time = "today";
+    var r = ES6.templateString('Hello ${name}, how are you ${time}?');
+  })()
+
+  r 应该返回的是：Hello Bob, how are you today?'
+  怎么才能让 templateString 函数取局部变量的值呢？？
+
+  -----------------------------------------
+
+  所以目前的API是：
+  var name = "Bob", time = "today";
+  var r1 = ES6.templateString({
+    name : name,
+    time : time
+  },'Hello ${name}, how are you ${time}?');
+  => 'Hello Bob, how are you today?'
+
+  var x = 1, y = 2;
+  var r2 = ES6.templateString({
+    x : x,
+    y : y
+  },'${ x } + ${ y } = ${ x + y }');
+  => 1 + 2 = 3
 */
 ES6.templateString = function(data, ts){
   var reg = /\${([^${}]+)}/g,
-			r = ts.match(reg),
-			result = ts;
-	
+      r = ts.match(reg),
+      result = ts;
+
   if( r == null ) return result;
 
-	function getObjKeys(obj){
-		var keys = [];
-		var hasOwnProperty = Object.prototype.hasOwnProperty;
-		
-		for(var key in obj){
-			if( hasOwnProperty.call(obj, key) ){
-				keys.push(key);
-			}
-		}
-		
-		return keys;
-	}
-	
+  function getObjKeys(obj){
+    var keys = [];
+    var hasOwnProperty = Object.prototype.hasOwnProperty;
+    
+    for(var key in obj){
+      if( hasOwnProperty.call(obj, key) ){
+        keys.push(key);
+      }
+    }
+    
+    return keys;
+  }
+
   result = ts.replace(reg, function(x, y){
-		try{
-			y = y.trim();
-			var value = data[y];
+    try{
+      y = y.trim();
+      var value = data[y];
 
-			if( value == undefined ){
-				var keys = getObjKeys(data);		
-				var keyStr = '';
-				
-				keys.forEach(function(v){
-					keyStr += "("+ v +")";
-				});
-				keyStr = "[" + keyStr + "]";
+      if( value == undefined ){
+        var keys = getObjKeys(data);		
+        var keyStr = '';
+        
+        keys.forEach(function(v){
+          keyStr += "("+ v +")";
+        });
+        keyStr = "[" + keyStr + "]";
 
-				var c = y.replace(new RegExp(keyStr, "g"), function(a){
-					return data[a];
-				});
-				
-				return eval(c);
-			}else{
-				return value;
-			}
-		}catch(e){
-			return x;
-		}
-	});
-	
-	return result;
-	
+        var c = y.replace(new RegExp(keyStr, "g"), function(a){
+          return data[a];
+        });
+        
+        return eval(c);
+      }else{
+        return value;
+      }
+    }catch(e){
+      return x;
+    }
+  });
+
+  return result;
+
 }
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 
