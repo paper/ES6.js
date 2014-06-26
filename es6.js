ec44682970408b13a3c5a4a449fcedc2a79a1086
paper <jue_paper@126.com>
2014-06-26 19:33:21 +0800

    0.1.2

--- es6.js
+++ es6.js
@@ -8,6 +8,40 @@ var ES6 = {}
 
 /**
   ------------------------------------------------------------------------
+  base 
+  一些会用到的基础函数 
+  
+  1. type
+  2. object keys
+  --------------------------------------------------------------------------
+*/
+
+var _TYPE = ["String", "Number", "Function", "Boolean", "Object", "Array"];
+_TYPE.forEach(function(v){
+  ES6["is" + v] = function(para){
+    return Object.prototype.toString.call(para) == "[object "+ v +"]";
+  }
+});
+
+Object.prototype.keys = function(){
+  if( !ES6.isObject(this) ) return false;
+  
+  var keys = [];
+  
+  for(var key in this){
+    // or => Object.prototype.hasOwnProperty.call(this, key)
+    if( this.hasOwnProperty(key) ){
+      keys.push(key);
+    }
+  }
+  
+  return keys;
+}
+
+
+
+/**
+  ------------------------------------------------------------------------
   字符串的扩展  
 
   1. contains(), startsWith(), endsWith()
@@ -29,8 +63,8 @@ String.prototype.startsWith = function(str, atIndex){
   return index == atIndex;
 }
 
-//endsWith的行为与其他两个方法有所不同。
-//它针对前n个字符，而其他两个方法针对从第n个位置直到字符串结束。
+// endsWith的行为与其他两个方法有所不同。
+// 它针对前n个字符，而其他两个方法针对从第n个位置直到字符串结束。
 String.prototype.endsWith = function(str, atIndex){
   var thisLen = this.length;
   var strLen = str.length;
@@ -100,26 +134,13 @@ ES6.templateString = function(data, ts){
 
   if( r == null ) return result;
 
-  function getObjKeys(obj){
-    var keys = [];
-    var hasOwnProperty = Object.prototype.hasOwnProperty;
-    
-    for(var key in obj){
-      if( hasOwnProperty.call(obj, key) ){
-        keys.push(key);
-      }
-    }
-    
-    return keys;
-  }
-
   result = ts.replace(reg, function(x, y){
     try{
       y = y.trim();
       var value = data[y];
 
       if( value == undefined ){
-        var keys = getObjKeys(data);		
+        var keys = data.keys();		
         var keysStr = '';
         
         keys.forEach(function(v){
@@ -143,6 +164,173 @@ ES6.templateString = function(data, ts){
   return result;
 
 }
+
+/**
+  ------------------------------------------------------------------------
+  数值的扩展  
+
+  1. 二进制和八进制表示法
+  2. Number.isFinite(), Number.isNaN()
+  3. Number.parseInt(), Number.parseFloat()
+  4. Number.isInteger()和安全整数
+  --------------------------------------------------------------------------
+*/
+
+// Number.parse("0b111110111") === 503   //true
+// Number.parse("0o767") === 503         // true
+Number.parse = function(str){
+  var preData = {
+        "0b" : 2,
+        "0o" : 8,
+        "0x" : 16
+      },
+      len = str.length,
+      preStr = str.slice(0,2),
+      lastStr = str.slice(2, len);
+      
+  if( preData[preStr] ){
+    return parseInt(lastStr, preData[preStr]);
+  }else{
+    return parseInt(str, 10);
+  }
+}
+
+// 实测是返回undefined，不是false。但文档说的确是false
+// => http://people.mozilla.org/~jorendorff/es6-draft.html#sec-number.isfinite
+Number.isFinite = function(n){
+  if( !ES6.isNumber(n) ) return false;
+  return isFinite(n);
+}
+
+Number.isNaN = function(n){
+  if( !ES6.isNumber(n) ) return false;
+  return isNaN(n);
+}
+
+Number.parseInt = function(n, ary){
+  return parseInt(n, ary);
+}
+
+Number.parseFloat = function(n){
+  return parseFloat(n);
+}
+
+Number.isInteger = function(n){
+  if( !ES6.isNumber(n) || Number.isNaN(n) ) return false;
+  
+  return n === parseInt(n, 10);
+}
+
+Number.MAX_SAFE_INTEGER = Math.pow(2,53) - 1;
+
+Number.MIN_SAFE_INTEGER = -Math.pow(2,53) + 1;
+
+Number.isSafeInteger = function(n){
+  if( !Number.isInteger(n)
+      || n < Number.MIN_SAFE_INTEGER
+      || n > Number.MAX_SAFE_INTEGE ) return false;
+  
+  return true;
+}
+
+
+/**
+  ------------------------------------------------------------------------
+  数组的扩展  
+
+  1. Array.from()
+  2. Array.of()
+  3. 数组实例的find()和findIndex()
+  4. 数组实例的fill()
+ *5. 数组实例的entries()，keys()和values() -> 返回一个遍历器 (不好搞)
+ *6. 数组推导 (不好搞)
+ *7. Array.observe()，Array.unobserve()
+     -> https://github.com/mennovanslooten/Observable-Arrays/blob/master/js/underscore.observable.js
+  --------------------------------------------------------------------------
+*/
+
+Array.from = function(arrLike, cb){
+  var result = Array.prototype.slice.call(arrLike);
+  
+  return ES6.isFunction(cb) ? result.map( cb ) : result;
+}
+
+Array.of = function(){
+  var result = [];
+  var argArr = Array.from(arguments);
+  
+  argArr.forEach(function(v){
+    result.push( v );
+  });
+  
+  return result;
+}
+
+Array.prototype.findIndex = function(cb, thisArg){
+  thisArg = thisArg || null;
+  
+  for(var i = 0, len = this.length; i<len; i++){
+    if( cb.call(thisArg, this[i], i, this) === true ){
+      return i;
+    }
+  }
+  
+  return -1;
+}
+
+Array.prototype.find = function(cb, thisArg){
+  var index = this.findIndex(cb, thisArg);
+  return index === -1 ? undefined : this[index];
+}
+
+Array.prototype.fill = function(n, begin, end){
+  var result = this.concat();
+  var len = result.length;
+  
+  begin = typeof begin == "undefined" ? 0 : begin;
+  end = typeof end == "undefined" ? len : end;
+  
+  if(begin < 0){ begin = len + begin; }
+  if(end < 0){ end = len + end; }
+  
+  for(var i = begin; i < end ; i++){
+    result[i] = n;
+  }
+  
+  return result;
+}
+
+
+/**
+  ------------------------------------------------------------------------
+  对象的扩展  
+
+  1. Object.is()
+  2. Object.assign()
+  3. proto属性，Object.setPrototypeOf()，Object.getPrototypeOf()
+  4. 增强的对象写法
+  5. 属性名表达式
+  6. Symbol
+  7. Proxy
+  8. Object.observe()，Object.unobserve()
+  --------------------------------------------------------------------------
+*/
+
+Object.is = function(a, b){
+  // 有一个 +0 , 有一个 -0
+  if( (a === 0 && b === 0) && 1/a !== 1/b ){
+    return false;
+  }
+  
+  //a=NaN, b=NaN
+  if( ES6.isNumber(a) && ES6.isNumber(b) && isNaN(a) && isNaN(b) ){
+    return true;
+  }
+  
+  return a === b;
+}
+
+
 
 
 
